f29e337a8e6bb0225d1d4175e4acf2acca1e2605
xwp <xwp_fullstack@163.com>
2019-03-30 05:07:33 +0800

    定义mysql数据库的链接和收取配置信息到config文件中

---
+++ config.py
@@ -0,0 +1,16 @@
+#导入redis数据库
+from redis import StrictRedis
+class Config:
+    #设置secret key 密钥值,通过os.urandom和base64.b64encode（）来实现
+    SECRET_KEY = '0FkqKfUFJcBQbfveExNUdbuAILmAxm6WXyWSm7uCsuzL48sRArlIR+k6b8GI'
+        #设置链接数据库名称,
+    SESSION_TYPE = 'redis'
+        #实例strictredis方法，并传入链接数据库的ip默认端口为6379
+    SESSION_REDIS = StrictRedis(host='127.0.0.1')
+        #设置签证
+    SESSION_USE_SIGNER = True
+        #设置数据库的过期时间值
+    PERMANENT_SESSION_LIFETIME = 86400
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@localhost/info'
+        # 动态追踪修改设置，如未设置只会提示警告
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
--- manage.py
+++ manage.py
@@ -2,21 +2,12 @@
 from flask import Flask,session
 #导入Session类来指定数据库的链接等操作
 from flask_session import Session
-#导入redis数据库
-from redis import StrictRedis
+from config import Config
 
 #实例app对象
 app = Flask(__name__)
-#设置secret key 密钥值,通过os.urandom和base64.b64encode（）来实现
-app.config['SECRET_KEY'] = '0FkqKfUFJcBQbfveExNUdbuAILmAxm6WXyWSm7uCsuzL48sRArlIR+k6b8GI'
-#设置链接数据库名称,
-app.config['SESSION_TYPE'] = 'redis'
-#实例strictredis方法，并传入链接数据库的ip默认端口为6379
-app.config['SESSION_REDIS'] = StrictRedis(host='127.0.0.1')
-#设置签证
-app.config['SESSION_USE_SIGNER'] = True
-#设置数据库的过期时间值
-app.config['PERMANENT_SESSION_LIFETIME'] = 86400
+#把配置文件添加到config中
+app.config.from_object(Config)
 # 使用Session类
 Session(app)
 @app.route('/')
