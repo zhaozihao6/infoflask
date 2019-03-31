a36a274bc449c1cba2b0358b3ac32a4ac9422ebe
xwp <xwp_fullstack@163.com>
2019-04-01 06:17:49 +0800

    实例关联sqlalchemy,和项目日志集成

--- config.py
+++ config.py
@@ -1,5 +1,23 @@
 #导入redis数据库
+
+
 from redis import StrictRedis
+# 设置日志的记录等级
+import logging
+from logging.handlers import RotatingFileHandler
+logging.basicConfig(level=logging.DEBUG) # 调试debug级
+# 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024*1024*100, backupCount=10)
+# 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+# 为刚创建的日志记录器设置日志记录格式
+file_log_handler.setFormatter(formatter)
+# 为全局的日志工具对象（flask app使用的）添加日志记录器
+logging.getLogger().addHandler(file_log_handler)
+
+
+
+
 class Config:
     #设置secret key 密钥值,通过os.urandom和base64.b64encode（）来实现
     SECRET_KEY = '0FkqKfUFJcBQbfveExNUdbuAILmAxm6WXyWSm7uCsuzL48sRArlIR+k6b8GI'
--- info/__init__.py
+++ info/__init__.py
@@ -1,13 +1,22 @@
+#导入flask类
 from flask import Flask
+#导入Session类
 from flask_session import Session
-from config import Config,config_dict
+#导入配置字典
+from config import config_dict
+#导入flask_sqlalchemy下的SQLAlchemy类
+from flask_sqlalchemy import SQLAlchemy
 #创建create_app类,设置形参
+# 实力sqlalchemy对象
+db = SQLAlchemy()
 def create_app(schema_name):
     app = Flask(__name__)
     #config_dict字典中的key值映射value类,而类又继承与Config类
     app.config.from_object(config_dict[schema_name])
     # 使用Session类
     Session(app)
+    #建立数据库和app的关联
+    db.init_app(app)
     #导入蓝图对象并注册蓝图，注意这个要写在Session(app)下面，避免循环导包
     from info.modules.news import news_blue
     #注册蓝图
